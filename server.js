983d7fff3b1b7ea79b97678902d57a3cd0b980af
Renat Kovshikov <renat.kovshikov@gmail.com>
2023-12-07 17:17:26 +0200

    week 15

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require ("express");
 const axios = require ("axios");
-
+const http = require ("http");
 const app = express ();
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.get("/", (req, res)=>{
     let url = "https://api.themoviedb.org/3/movie/872585?api_key=f4e7aff15bee14e0c9460fa717bbb7a1";
@@ -81,6 +82,51 @@ app.post("/search", (req, res) => {
         res.render("search", {movieDetails: movieData});
     }));
 
+});
+
+app.post('/getmovie', (req, res) => {
+	const movieToSearch =
+		req.body.queryResult && req.body.queryResult.parameters && req.body.queryResult.parameters.movie
+			? req.body.queryResult.parameters.movie
+			: '';
+
+	const reqUrl = encodeURI(
+		`http://www.omdbapi.com/?t=${movieToSearch}&apikey=bc403151`
+	);
+	http.get(
+		reqUrl,
+		responseFromAPI => {
+			let completeResponse = ''
+			responseFromAPI.on('data', chunk => {
+				completeResponse += chunk
+			})
+			responseFromAPI.on('end', () => {
+				const movie = JSON.parse(completeResponse);
+                if (!movie || !movie.Title) {
+                    return res.json({
+                        fulfillmentText: 'Sorry, we could not find the movie you are asking for.',
+                        source: 'getmovie'
+                    });
+                }
+
+				let dataToSend = movieToSearch;
+				dataToSend = `${movie.Title} was released in the year ${movie.Year}. It is directed by ${
+					movie.Director
+				} and stars ${movie.Actors}.\n Here some glimpse of the plot: ${movie.Plot}.`;
+
+				return res.json({
+					fulfillmentText: dataToSend,
+					source: 'getmovie'
+				});
+			})
+		},
+		error => {
+			return res.json({
+				fulfillmentText: 'Could not get results at this time',
+				source: 'getmovie'
+			});
+		}
+	)
 });
 
 app.listen(process.env.PORT || 3000, ()=>{
